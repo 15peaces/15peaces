458b39088cbb7b18a5060afcc5f0601d4624bd22
15peaces <15peaces@d4ce06a9-040b-c14e-3734-79552116bd41>
2014-02-02 16:09:17 +0000

    *15cp update 0.08: ------------------ -Updated style.css -Added Download Page link to navigation bar & configutation file. -Added Forum link to navigation bar & configutation file. -Added related functions so Login / Logout are now working

--- Ragnarok Online/15cp/config.php
+++ Ragnarok Online/15cp/config.php
@@ -1,6 +1,6 @@
 <?
 	/*
-		Configuration file for 15CP 0.04
+		Configuration file for 15CP 0.08
 	
 		Legend:
 			(*1) = Please use  = 'value'; format here!
@@ -14,10 +14,12 @@
 	//SQL related
 	$conf['sql_rag_ip'] 	= '127.0.0.1'; //Your RO-Servers SQL IP (*1)
 	$conf['sql_rag_user'] 	= 'root'; //Your RO-Servers SQL Username (*1)
-	$conf['sql_rag_pass'] 	= 'pass'; //Your RO-Servers SQL Password (*1)
+	$conf['sql_rag_pass'] 	= 'password'; //Your RO-Servers SQL Password (*1)
 	$conf['rag_db']			= 'ragnarok'; //Your RO-Servers Maindatabase (*1)
 	
 	//Mainserver configs
+	
+	//NOTE: It is recommoned to use a DNS. If you use 127.0.0.1 or localhost, it'll maybe make the cp really slow...
 	$conf['serverip_log'] = '127.0.0.1'; //Your RO- Loginserver IP (*1)
 	$conf['serverip_char'] = '127.0.0.1'; //Your RO- Characterserver IP (*1)
 	$conf['serverip_map'] = '127.0.0.1'; //Your RO- Mapserver IP (*1)
@@ -28,12 +30,13 @@
 	
 	// ------------------------------------------------------------------------------------------
 	// Plugins:
-
+	
 	//Link configuration:
 	$config['plugin.homelink'] = ''; //Link to Servers Homepage or empty to disable
+	$config['plugin.downloadlink'] = ''; //Link to Servers Downloadpage or empty to disable
+	$config['plugin.forumlink'] = ''; //Link to Servers Downloadpage or empty to disable
 	
 	// Facebook Plugin configuration:
 	$config['plugin.facebook.activate'] = true; //Activate Facebook-Plugin (*2)
 	$config['plugin.facebook.url'] 		= 'YOUR PAGE URL'; //(*1)
-	
 ?>
--- Ragnarok Online/15cp/functions.php
+++ Ragnarok Online/15cp/functions.php
@@ -74,4 +74,60 @@
 			return(false);
 		}
 	}
+
+	function deleteCookie($cookie)
+	{
+		setcookie($cookie,"",time()+60*60*24*30);
+		return;
+	}
+
+	function login($ID,$pass,$stay,$page)
+	{
+		global $conf;
+		$con = @mysql_pconnect($conf['sql_rag_ip'],$conf['sql_rag_user'],$conf['sql_rag_pass']);
+		if(!$con){
+				echo('<p style=color:red>mySQL-Error: ' . mysql_error().'</p><a href=index.php style="color:blue">back</a>');
+				return;
+				mysql_close($con);
+			}
+		mysql_select_db($mainconf['sql_db'], $con);
+		$out = mysql_query("SELECT userid,user_pass FROM login",$con);
+		if(!$out){
+				echo('<p style=color:red>mySQL-Error: ' . mysql_error().'</p><a href=index.php style="color:blue">back</a>');
+				return;
+				mysql_close($con);
+				
+			}
+		while($row = mysql_fetch_array($out))
+		{
+			if ($row['userid'] == $ID){
+				if(md5($row['user_pass']) == $pass){
+					if(isset($stay)){
+						setcookie("ID",$ID,time()+60*60*24*30);
+						setcookie("pass",$pass,time()+60*60*24*30);
+					}else{
+						setcookie("ID",$ID,0);
+						setcookie("pass",$pass,0);
+					}
+					echo ('<meta http-equiv="refresh" content="0; URL=?site='.$page.'">');
+					return;
+					mysql_close($con);
+				}
+				return('<p style=color:red>USERNAME/PASSWORD WRONG!</p><a href=index.php style="color:blue">back</a>');
+				return;
+				mysql_close($con);
+			}
+		}
+		return('<p style=color:red>USERNAME/PASSWORD WRONG!</p><a href=index.php style="color:blue">back</a>');
+		return;
+		mysql_close($con);
+	}
+	
+	function logoff()
+	{
+		deleteCookie("ID");
+		deleteCookie("pass");
+		echo '<meta http-equiv="refresh" content="0; URL=?site=home">';
+		return;
+	}
 ?>
